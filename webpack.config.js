743dc14e50ac740ae9c22039d2c36e32dfe04b11
JuanCa <144077089+Shadowfiend2504@users.noreply.github.com>
2026-05-19 18:54:50 -0500

    feat: actualización para despliegue estático en Vercel. Generación de public/index.html y copiado de assets/css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,46 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    clean: true, // Limpia la carpeta de salida antes de generar los archivos
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' },
+        { from: 'src/css', to: 'css' },
+      ],
+    }),
+  ],
   devtool: 'source-map',
-  mode: 'development',
+  mode: 'production',
 };
